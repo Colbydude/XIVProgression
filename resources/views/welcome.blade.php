b3fefe16e72a2cab1eae61fb99805093da6801ca
Colby Terry <colbydude@gmail.com>
2017-08-02 22:13:49 -0700

    Update welcome background.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,7 +17,7 @@
             padding: 0;
             width: 100%;
             height: 100%;
-            background-image: url('../img/heavensward-bg.jpg');
+            background-image: url('../img/stormblood-bg.jpg');
             background-repeat: no-repeat;
             background-size: cover;
             color: white;
