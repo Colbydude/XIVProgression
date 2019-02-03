a4df00318bc3e9830de4fdf86ec4d5a971834cbf
Colby Terry <colby.terry@slalom.com>
2019-02-03 11:57:54 -0800

    Upgrade app to Laravel 5.7.

--- app/Character.php
+++ app/Character.php
@@ -6,6 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Character extends Model
 {
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
     protected $fillable = [
         'lodestone_id', 'name', 'server'
     ];
